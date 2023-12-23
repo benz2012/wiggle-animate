e8fc35f10c7c70f1b9b3cbc6a95fdeb1cb712cc9
Ben Zenker <imbenzenker@gmail.com>
2023-12-23 14:31:12 -0700

    adds image preloading ability

--- src/utility/fonts.js
+++ src/utility/fonts.js
@@ -1,22 +1,26 @@
 /* NOTE: This file must be imported somewhere in order to run it's top level initialization */
+import missingPreview from '../assets/missingPreview.png'
 
 const STATIC_ASSETS_PATH = './_subapps/animate'
 
 let fontListData = {}
 const getFontListData = () => fontListData
 
-if (process.env.NODE_ENV === 'production') {
-  fetch(`${STATIC_ASSETS_PATH}/${process.env.REACT_APP_FONT_DATA_FILE}`)
-    .then((res) => res.json())
-    .then((data) => {
-      fontListData = data
-    })
-} else {
-  // eslint-disable-next-line
-  import('../hidden/font-data-google.json')
-    .then((module) => {
-      fontListData = module.default
-    })
+const preloadSetOfFontImages = (fontList, onEachImageLoaded) => {
+  const { imageURLPrefix } = getFontListData()
+  let numLoaded = 0
+
+  fontList.forEach((fontData) => {
+    if (fontData.i === '0') return
+    const image = new Image()
+
+    image.onload = () => {
+      numLoaded += 1
+      onEachImageLoaded(numLoaded)
+    }
+
+    image.src = `${imageURLPrefix}${fontData.i}`
+  })
 }
 
 const loadFont = (fontName) => {
@@ -36,7 +40,25 @@ const loadFont = (fontName) => {
   })
 }
 
+// Top-Level Initialization Code -- leave this at bottom of module
+if (process.env.NODE_ENV === 'production') {
+  fetch(`${STATIC_ASSETS_PATH}/${process.env.REACT_APP_FONT_DATA_FILE}`)
+    .then((res) => res.json())
+    .then((data) => {
+      fontListData = data
+    })
+} else {
+  // eslint-disable-next-line
+  import('../hidden/font-data-google.json')
+    .then((module) => {
+      fontListData = module.default
+    })
+}
+const missingPreviewImage = new Image()
+missingPreviewImage.src = missingPreview
+
 export {
   getFontListData,
   loadFont,
+  preloadSetOfFontImages,
 }
