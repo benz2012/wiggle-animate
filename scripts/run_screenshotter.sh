8f94ffa309fb004eb69b4e31f3afed504fc62ed6
Ben Zenker <imbenzenker@gmail.com>
2024-02-28 18:20:45 -0700

    src code mount to eliminate docker builds

--- scripts/run_screenshotter.sh
+++ scripts/run_screenshotter.sh
@@ -2,4 +2,8 @@
 
 COMMAND="npm run test -- --update-snapshots"
 
-docker run --rm -v ${PWD}/tests:/usr/local/app/tests playwright-linux-screenshotter $COMMAND
+docker run --rm \
+  -v ${PWD}/src:/usr/local/app/src \
+  -v ${PWD}/tests:/usr/local/app/tests \
+  -v ${PWD}/public:/usr/local/app/public \
+  playwright-linux-screenshotter $COMMAND
